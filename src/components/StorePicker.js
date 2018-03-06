31bfccd8a5b2c96053471fce7fbb08f201a351f5
byoung <breen.young@gmail.com>
2018-03-06 12:38:27 -0400

    Completed lesson 12

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,13 +3,31 @@ import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component
 {
+    myInput = React.createRef();
+
+    // Need to do this as a prop so we get 'this' context as our own methods don't get bound
+    goToStore = (event) =>
+    {
+        event.preventDefault();
+
+        // Get the input value
+        const storeName = this.myInput.value.value;
+
+        // Use Router component (it's a parent up the component chain) to switch to another page
+        this.props.history.push(`/store/${storeName}`);
+    };
+
     render()
     {
         return (
             <Fragment>
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please enter a Store</h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                    <input type="text"
+                           ref={this.myInput}
+                           required
+                           placeholder="Store Name"
+                           defaultValue={getFunName()}/>
                     <button type="submit">Visit Store</button>
                 </form>
             </Fragment>
